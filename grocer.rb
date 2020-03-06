db159554f20466cab2d7741c6e5ca02193956215
George Cooper <coopsgeorge@gmail.com>
2020-03-06 20:18:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ while coupon_index < coupons.length
   coupon_item_name = coupons[coupon_index][:item]
   coupon = coupons[coupon_index]
   binding.pry
-  #find_item_by_name_in_collection(name, collection)
+  cart_item = find_item_by_name_in_collection(coupon_item_name, cart)
   new_coupon = {:item => "#{coupon_item_name} W/COUPON", :price => coupon[:cost]/coupon[:num], :clearance => true, :count => coupon[:num]}
 
   cart.push(new_coupon)
