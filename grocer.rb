6bc07cb2a2fef318a6b12008b2297cf8ec8ce8ce
George Cooper <coopsgeorge@gmail.com>
2020-03-06 20:58:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,7 +72,7 @@ def apply_clearance(cart)
 item_index = 0
 while item_index < cart.item_index
   if cart[item_index][:price] = (cart[item_index][:price] - (cart[item_index][:price]*20)).round(2)
-
+    item_index += 1
   end
 end
 end
